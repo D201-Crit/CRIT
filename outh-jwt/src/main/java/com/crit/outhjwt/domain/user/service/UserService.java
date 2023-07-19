fe1e787f8647294a7dc796b18bf0e0852b2ccc77
rlvy98 <rlvy98@naver.com>
2023-07-19 17:44:05 +0900

    init

---
+++ outh-jwt/src/main/java/com/crit/outhjwt/domain/user/service/UserService.java
@@ -0,0 +1,45 @@
+package com.crit.outhjwt.domain.user.service;
+
+import com.crit.outhjwt.domain.user.entity.Role;
+import com.crit.outhjwt.domain.user.entity.User;
+import com.crit.outhjwt.domain.user.dto.UserSignUpDto;
+import com.crit.outhjwt.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void signUp(UserSignUpDto userSignUpDto) throws Exception {
+
+        if (userRepository.findByEmail(userSignUpDto.getEmail()).isPresent()) {
+            throw new Exception("이미 존재하는 이메일입니다.");
+        }
+
+        if (userRepository.findByNickname(userSignUpDto.getNickname()).isPresent()) {
+            throw new Exception("이미 존재하는 닉네임입니다.");
+        }
+
+        User user = User.builder()
+                .email(userSignUpDto.getEmail())
+                .password(userSignUpDto.getPassword())
+                .nickname(userSignUpDto.getNickname())
+                .age(userSignUpDto.getAge())
+                .city(userSignUpDto.getCity())
+                .role(Role.USER)
+                .build();
+
+        user.passwordEncode(passwordEncoder);
+        userRepository.save(user);
+    }
+}
+
+
