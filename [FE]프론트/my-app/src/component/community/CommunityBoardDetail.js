1f63eff45275ed6b2f6d52d249094776d00e6bcf
jihun <hotchapa@naver.com>
2023-08-03 01:50:06 +0900

    feat : 커뮤니티 작성 모달 생성 및 좋아요 기능 추가

--- [FE]프론트/my-app/src/component/community/CommunityBoardDetail.js
+++ [FE]프론트/my-app/src/component/community/CommunityBoardDetail.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { SHr, SCommunityWrapper, SEmpty, SBoardArticleCol,SBoardArticleRow } from '../../styles/pages/SCommunityPage';
 import axios from 'axios';
+import {
+  SHr,
+  SCommunityWrapper,
+  SEmpty,
+  SBoardArticleCol,
+  SBoardArticleRow,
+} from '../../styles/pages/SCommunityPage';
 import { api, getNewAccessToken } from '../../api/api';
 
 const API_BASE_URL = 'http://localhost:8080/api/boards';
@@ -14,24 +20,16 @@ const CommunityBoardDetail = ({ classification }) => {
   const [newBoard, setNewBoard] = useState({
     title: '',
     content: '',
-    classification: '',
+    classification: classification,
     writer: user.nickname,
   }); // 새로운 게시글 입력 폼 상태
   const [editBoard, setEditBoard] = useState(null); // 수정할 게시글 상태
-  const [filteredBoards, setFilteredBoards] = useState([]); // classification에 따라 필터링된 게시글 배열 추가
+  const [filteredBoards, setFilteredBoards] = useState([]); // classification에 따라 필터링된 게시글 배열
+  const [showModal, setShowModal] = useState(false); // 모달 표시 여부 상태
 
   useEffect(() => {
     fetchBoards();
   }, [classification, page]); // classification과 page를 useEffect의 의존성(dependency)으로 추가합니다.
-
-
-  const handleGoToDetail = (id) => {
-    // 자세히 보기 버튼을 누를 때 해당 경로로 이동합니다.
-    // classification에 맞는 경로로 이동하도록 수정해주세요.
-    // 예를 들어, /CommunityBoardPage/:classification 경로로 이동하도록 합니다.
-    window.location.href = `/CommunityBoardPage/${classification}/${id}`;
-  };
-
 
   // 서버에서 모든 게시글을 가져오는 함수
   const fetchBoards = async () => {
@@ -60,6 +58,30 @@ const CommunityBoardDetail = ({ classification }) => {
     }
   };
 
+  const handleGoToDetail = (id) => {
+    // 자세히 보기 버튼을 누를 때 해당 경로로 이동합니다.
+    // classification에 맞는 경로로 이동하도록 수정해주세요.
+    // 예를 들어, /CommunityBoardPage/:classification 경로로 이동하도록 합니다.
+    window.location.href = `/CommunityBoardPage/${classification}/${id}`;
+  };
+
+  const handleCreatePost = () => {
+    // 새로운 게시글 작성 모달 열기
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    // 모달 닫기
+    setShowModal(false);
+    // 모달 닫을 때 입력 폼 초기화
+    setNewBoard({
+      title: '',
+      content: '',
+      classification: '',
+      writer: user.nickname,
+    });
+  };
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setNewBoard({
@@ -68,27 +90,18 @@ const CommunityBoardDetail = ({ classification }) => {
     });
   };
 
-  const handleBoardSubmit = async (event) => {
+  const handlePostSubmit = async (event) => {
     event.preventDefault();
     try {
-      if (editBoard) {
-        const response = await axios.put(`${API_BASE_URL}/update/${editBoard.id}`, newBoard, {
-          headers: {
-            Authorization: `Bearer ${user.accessToken}`,
-          },
-        });
-        setBoards((prevBoards) =>
-          prevBoards.map((board) => (board.id === response.data.data.id ? response.data.data : board))
-        );
-        setEditBoard(null);
-      } else {
-        const response = await axios.post(`${API_BASE_URL}/write`, newBoard, {
-          headers: {
-            Authorization: `Bearer ${user.accessToken}`,
-          },
-        });
-        setBoards([...boards, response.data.data]);
-      }
+      const response = await axios.post(`${API_BASE_URL}/write`, newBoard, {
+        headers: {
+          Authorization: `Bearer ${user.accessToken}`,
+        },
+      });
+      // 새로운 게시글 작성 성공 시, 모달 닫기 및 게시글 목록에 추가
+      setShowModal(false);
+      setBoards([...boards, response.data.data]);
+      // 입력 폼 초기화
       setNewBoard({
         title: '',
         content: '',
@@ -96,35 +109,8 @@ const CommunityBoardDetail = ({ classification }) => {
         writer: user.nickname,
       });
     } catch (error) {
-      console.log(newBoard);
       console.error(error);
       console.log('게시글 작성 실패');
-    }
-  };
-
-  const handleEditClick = (board) => {
-    setEditBoard(board);
-    setNewBoard({
-      title: board.title,
-      content: board.content,
-      classification: board.classification,
-      writer: board.writer,
-    });
-  };
-
-  const handleDeleteClick = async (boardId) => {
-    try {
-      await api.delete(`${API_BASE_URL}/delete/${boardId}`, {
-        headers: {
-          Authorization: `Bearer ${user.accessToken}`,
-        },
-      });
-      setBoards((prevBoards) => prevBoards.filter((board) => board.id !== boardId));
-      setFilteredBoards((prevBoards) => prevBoards.filter((board) => board.id !== boardId));
-      console.log('삭제 성공');
-    } catch (error) {
-      console.error(error);
-      console.log('게시글 삭제 실패');
     }
   };
 
@@ -135,27 +121,110 @@ const CommunityBoardDetail = ({ classification }) => {
   const handleNextPage = () => {
     setPage((prevPage) => Math.min(prevPage + 1, totalPages - 1));
   };
+  const handleLikeClick = async (boardId) => {
+    try {
+      const response = await api.post(`${API_BASE_URL}/likes/${boardId}`, null, {
+        headers: {
+          Authorization: `Bearer ${user.accessToken}`,
+        },
+      });
+      // 좋아요 성공 시 서버에서 받은 데이터를 기반으로 해당 게시글에 좋아요를 표시하도록 업데이트합니다.
+      setBoards((prevBoards) =>
+        prevBoards.map((board) =>
+          board.id === boardId
+            ? {
+                ...board,
+                liked: true,
+                likesCount: response.data.data.likesCount,
+              }
+            : board
+        )
+      );
+    } catch (error) {
+      console.error(error);
+      console.log('게시글 좋아요 실패');
+    }
+  };
+
+  const handleUnlikeClick = async (boardId) => {
+    try {
+      const response = await api.delete(`${API_BASE_URL}/likes/${boardId}`, {
+        headers: {
+          Authorization: `Bearer ${user.accessToken}`,
+        },
+      });
+      // 좋아요 삭제 성공 시 서버에서 받은 데이터를 기반으로 해당 게시글에 좋아요를 해제하도록 업데이트합니다.
+      setBoards((prevBoards) =>
+        prevBoards.map((board) =>
+          board.id === boardId
+            ? {
+                ...board,
+                liked: false,
+                likesCount: response.data.data.likesCount,
+              }
+            : board
+        )
+      );
+    } catch (error) {
+      console.error(error);
+      console.log('게시글 좋아요 취소 실패');
+    }
+  };
   return (
     <div>
       <h1>{classification}</h1>
       <SHr />
       <SEmpty />
-      <SCommunityWrapper >
-       
-          {filteredBoards.map((board) => (
-            <SBoardArticleCol key={board.id}>
-              <SBoardArticleRow >
-              <h3 onClick={()=> handleGoToDetail(board.id)} >{board.title}</h3>
+      <SCommunityWrapper>
+        {filteredBoards.map((board) => (
+          <SBoardArticleCol key={board.id}>
+            <SBoardArticleRow>
+              <h3 onClick={() => handleGoToDetail(board.id)}>{board.title}</h3>
               <p>작성자: {board.writer}</p>
               <p>조회수: {board.views}</p>
-              </SBoardArticleRow>
-              <div>
-                <button onClick={() => handleEditClick(board)}>수정</button>
-                <button onClick={() => handleDeleteClick(board.id)}>삭제</button>
-              </div>
-            </SBoardArticleCol>
-          ))}
+            </SBoardArticleRow>
+            <div>
+              {board.liked ? (
+                <button onClick={() => handleUnlikeClick(board.id)}>좋아요 취소</button>
+              ) : (
+                <button onClick={() => handleLikeClick(board.id)}>좋아요</button>
+              )}
+            </div>
+          </SBoardArticleCol>
+        ))}
+        <button onClick={handleCreatePost}>게시글 작성</button>
       </SCommunityWrapper>
+
+      {/* 게시글 작성 모달 */}
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={handleCloseModal}>
+              &times;
+            </span>
+            <form onSubmit={handlePostSubmit}>
+              <div>
+                <label>제목:</label>
+                <input type="text" name="title" value={newBoard.title} onChange={handleInputChange} />
+              </div>
+              <div>
+                <label>내용:</label>
+                <textarea name="content" value={newBoard.content} onChange={handleInputChange}></textarea>
+              </div>
+              <div>
+                <label>게시판 종류:</label>
+                <select
+                  name="classification"
+                  value={boards.classification}
+                  onChange={handleInputChange}
+                >
+                </select>
+              </div>
+              <button type="submit">작성 완료</button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- [FE]프론트/my-app/src/pages/community-article-detail-page.js
+++ [FE]프론트/my-app/src/pages/community-article-detail-page.js
@@ -1,4 +1,3 @@
-// CommunityBoardDetailPage.js
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
@@ -7,10 +6,11 @@ import { useSelector } from "react-redux";
 const API_BASE_URL = 'http://localhost:8080/api/boards';
 
 const CommunityArticleDetailPage = () => {
-  const user = useSelector((state) => state.users); // useSelector를 통해 userSlice의 상태를 가져옴
+  const user = useSelector((state) => state.users);
   const { classification, articleid } = useParams();
   const [board, setBoard] = useState({});
   const [newComment, setNewComment] = useState('');
+  const [isEditOpen, setIsEditOpen] = useState(false); // 수정 폼 표시 여부
 
   useEffect(() => {
     // fetchBoard와 fetchComments를 순차적으로 실행하기 위해 async 함수를 사용
@@ -24,7 +24,7 @@ const CommunityArticleDetailPage = () => {
 
   const fetchBoard = async () => {
     try {
-      const response = await axios.get(`${API_BASE_URL}/${articleid}`,{
+      const response = await axios.get(`${API_BASE_URL}/${articleid}`, {
         headers: {
           Authorization: `Bearer ${user.accessToken}`,
         },
@@ -32,12 +32,13 @@ const CommunityArticleDetailPage = () => {
       console.log(user.accessToken)
       console.log(articleid)
       const data = response.data.data;
-      setBoard({ ...data, comments: data.comments || [] }); // comments가 없을 경우 빈 배열로 초기화
+      setBoard({ ...data, comments: data.comments || [] });
     } catch (error) {
       console.error(error);
       console.log('게시글 조회 실패');
     }
   };
+
   const fetchComments = async () => {
     try {
       const response = await axios.get(`${API_BASE_URL}/comments/${articleid}`, {
@@ -51,11 +52,10 @@ const CommunityArticleDetailPage = () => {
       console.log('댓글 조회 실패');
     }
   };
-  
+
   const handleCommentChange = (event) => {
     setNewComment(event.target.value);
   };
-
 
   const handleCommentSubmit = async (event) => {
     event.preventDefault();
@@ -84,11 +84,10 @@ const CommunityArticleDetailPage = () => {
       console.log('댓글 작성 실패');
     }
   };
-  
 
   const handleCommentDelete = async (commentId) => {
     try {
-      await axios.delete(`${API_BASE_URL}/comments/${articleid}/${commentId}`,{
+      await axios.delete(`${API_BASE_URL}/comments/${articleid}/${commentId}`, {
         headers: {
           Authorization: `Bearer ${user.accessToken}`,
         },
@@ -101,49 +100,119 @@ const CommunityArticleDetailPage = () => {
     }
   };
 
+  const isMyComment = (comment) => {
+    return user.nickname === comment.writer;
+  };
+
+  const isMyBoard = () => {
+    return user.nickname === board.writer;
+  };
+
+  const handleEditClick = () => {
+    setIsEditOpen(true);
+  };
+
+  const handleEditCancel = () => {
+    setIsEditOpen(false);
+  };
+
+  const handleEditSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.put(
+        `${API_BASE_URL}/${articleid}`,
+        {
+          title: board.title,
+          content: board.content,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${user.accessToken}`,
+          },
+        }
+      );
+      setBoard({ ...board, title: response.data.data.title, content: response.data.data.content });
+      setIsEditOpen(false);
+    } catch (error) {
+      console.error(error);
+      console.log('게시글 수정 실패');
+    }
+  };
+
+  const handleDeleteClick = async () => {
+    try {
+      await axios.delete(`${API_BASE_URL}/${articleid}`, {
+        headers: {
+          Authorization: `Bearer ${user.accessToken}`,
+        },
+      });
+      // 삭제 후 이전 페이지로 이동 또는 처리할 로직 추가
+    } catch (error) {
+      console.error(error);
+      console.log('게시글 삭제 실패');
+    }
+  };
+
   return (
-    // 만약 댓글이 하나라도 있다면 출력
     <div>
       {Object.keys(board).length > 0 ? (
         <div>
-          <h3>{board.title}</h3>
-          <p>{board.content}</p>
-          <p>작성자: {board.writer}</p>
-          <p>조회수: {board.views}</p>
-          {user.nickname === board.writer && (
+          {isEditOpen ? (
+            // 수정 폼 표시
+            <form onSubmit={handleEditSubmit}>
+              <input
+                type="text"
+                value={board.title}
+                onChange={(e) => setBoard({ ...board, title: e.target.value })}
+              />
+              <textarea
+                value={board.content}
+                onChange={(e) => setBoard({ ...board, content: e.target.value })}
+              />
+              <button type="submit">수정 완료</button>
+              <button onClick={handleEditCancel}>수정 취소</button>
+            </form>
+          ) : (
             <div>
-              <button>수정</button>
-              <button>삭제</button>
+              <h3>{board.title}</h3>
+              <p>{board.content}</p>
+              <p>작성자: {board.writer}</p>
+              <p>조회수: {board.views}</p>
+              {isMyBoard() && (
+                <div>
+                  <button onClick={handleEditClick}>수정</button>
+                  <button onClick={handleDeleteClick}>삭제</button>
+                </div>
+              )}
+              <hr />
+              <h4>댓글 작성</h4>
+              <form onSubmit={handleCommentSubmit}>
+                <input
+                  type="text"
+                  value={newComment}
+                  onChange={handleCommentChange}
+                  placeholder="댓글을 입력하세요"
+                />
+                <button type="submit">작성</button>
+              </form>
+              <hr />
+              <div>
+                <h4>댓글 목록</h4>
+                {board.comments.map((comment) => (
+                  <div key={comment.id}>
+                    <p>{comment.content}</p>
+                    <p>작성자: {comment.writer}</p>
+                    {isMyComment(comment) && (
+                      <button onClick={() => handleCommentDelete(comment.id)}>삭제</button>
+                    )}
+                  </div>
+                ))}
+              </div>
             </div>
           )}
-          <hr />
-          <h4>댓글 작성</h4>
-          <form onSubmit={handleCommentSubmit}>
-            <input
-              type="text"
-              value={newComment}
-              onChange={handleCommentChange}
-              placeholder="댓글을 입력하세요"
-            />
-            <button type="submit">작성</button>
-          </form>
-          <hr />
-          <div>
-            <h4>댓글 목록</h4>
-            {board.comments.map((comment) => (
-              <div key={comment.id}>
-                <p>{comment.content}</p>
-                <p>작성자: {comment.writer}</p>
-                {user.nickname === comment.writer && (
-                  <button onClick={() => handleCommentDelete(comment.id)}>삭제</button>
-                )}
-              </div>
-            ))}
-          </div>
         </div>
-         ) : (
-          // 만약 댓글이 하나도 없다면..
-          <div>작성된 댓글이 없습니다.</div>
+      ) : (
+        <div>작성된 댓글이 없습니다.</div>
       )}
     </div>
   );
