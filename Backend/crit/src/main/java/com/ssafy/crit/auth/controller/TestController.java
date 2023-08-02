9d4221c65701ed5d4ffa90314d7c8c6d3a14c93e
rlvy98 <rlvy98@naver.com>
2023-08-02 16:16:27 +0900

    feat: AWS S3 업로드 기능 추가

--- Backend/crit/src/main/java/com/ssafy/crit/auth/controller/TestController.java
+++ Backend/crit/src/main/java/com/ssafy/crit/auth/controller/TestController.java
@@ -1,14 +1,27 @@
 package com.ssafy.crit.auth.controller;
 
+import com.ssafy.crit.common.s3.S3Uploader;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequiredArgsConstructor
 public class TestController {
 
+    private final S3Uploader s3Uploader;
     @GetMapping("/test")
     public String getTest() {
         System.out.println("===================testing=====================");
         return "success Test";
     }
+
+    @PostMapping("/upload")
+    public String upload(@RequestPart("file") MultipartFile multipartFile) throws Exception {
+        return s3Uploader.uploadFiles(multipartFile, "shorts");
+    }
+
 }
