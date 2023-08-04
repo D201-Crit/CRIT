a476d5c3a5f598d37a2df932b497157b821a3a4b
rlvy98 <rlvy98@naver.com>
2023-08-04 16:20:28 +0900

    feat: response dto 수정

--- Backend/crit/src/main/java/com/ssafy/crit/shorts/controller/ShortsController.java
+++ Backend/crit/src/main/java/com/ssafy/crit/shorts/controller/ShortsController.java
@@ -1,21 +1,16 @@
 package com.ssafy.crit.shorts.controller;
 
-
 import com.ssafy.crit.auth.entity.User;
 import com.ssafy.crit.auth.jwt.JwtProvider;
-import com.ssafy.crit.shorts.dto.MainThumbnailDto;
+import com.ssafy.crit.message.response.Response;
 import com.ssafy.crit.shorts.dto.ShortsDto;
-import com.ssafy.crit.shorts.dto.ShortsResponseDto;
 import com.ssafy.crit.shorts.service.ShortsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @RestController
 @RequestMapping("/shorts")
@@ -26,36 +21,36 @@ public class ShortsController {
     private final JwtProvider jwtProvider;
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<ShortsResponseDto> create(@RequestPart(value="shortsDto") ShortsDto shortsDto, @RequestPart(value="file", required = false) MultipartFile file, HttpServletRequest request) throws Exception {
+    public Response<?> create(@RequestPart(value="shortsDto") ShortsDto shortsDto, @RequestPart(value="file", required = false) MultipartFile file, HttpServletRequest request) throws Exception {
         User user = jwtProvider.extractUser(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(shortsService.create(shortsDto, file, user));
+        return new Response<>("성공", "쇼츠 생성 완료", shortsService.create(shortsDto, file, user));
     }
 
     @GetMapping
-    public ResponseEntity<List<ShortsDto>> getAll() {
-        return ResponseEntity.ok(shortsService.getAll());
+    public Response<?> getAll() {
+        return new Response<>("성공", "전체 쇼츠 조회 완료", shortsService.getAll());
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ShortsDto> read(@PathVariable Long id) {
-        return ResponseEntity.ok(shortsService.get(id));
+    @GetMapping("/{shortsId}")
+    public Response<?> read(@PathVariable Long shortsId) {
+        return new Response<>("성공", "단건 쇼츠 조회 완료", shortsService.get(shortsId));
     }
 
-    @PutMapping(value= "/{id}",consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<ShortsDto> update(@PathVariable Long id, @RequestBody ShortsDto shortsDto) {
-        return ResponseEntity.ok(shortsService.update(id, shortsDto));
+    @PutMapping(value= "/{shortsId}",consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public Response<?> update(@PathVariable Long shortsId, @RequestBody ShortsDto shortsDto) {
+        return new Response<>("성공", "쇼츠 수정 완료", shortsService.update(shortsId, shortsDto));
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        shortsService.delete(id);
-        return ResponseEntity.noContent().build();
+    @DeleteMapping("/{shortsId}")
+    public Response<?> delete(@PathVariable Long shortsId) {
+        shortsService.delete(shortsId);
+        return new Response<>("성공", "쇼츠 삭제 완료", "삭제완료");
     }
 
     // 최신순, 좋아요순, 조회순 -> 메인 페이지에 썸네일 이미지
-    @GetMapping("/thumbnail")
-    public ResponseEntity<MainThumbnailDto> getMainThumbnail() {
-        return ResponseEntity.ok(shortsService.getMainThumbnail());
+    @GetMapping("/main")
+    public Response<?> getMainThumbnail() {
+        return new Response<>("성공", "메인 페이지 쇼츠 가져오기", shortsService.getMainThumbnail());
     }
 
 
