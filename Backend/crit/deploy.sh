7df96b3c3a7313afddd9b299bc4e41ac4b341ef7
김민석 <koo3435@gmail.com>
2023-08-04 10:24:46 +0900

    Docs deploy.sh update

--- Backend/crit/deploy.sh
+++ Backend/crit/deploy.sh
@@ -16,6 +16,9 @@ docker stop crit_be
  
 echo '컨테이너 삭제'
 docker rm crit_be
+
+echo '쓰레기 이미지 삭제'
+docker images -f "dangling=true" -q | xargs docker rmi
  
 echo '컨테이너 실행'
 docker run -p 8080:8080 -v /home/ubuntu:/config --name crit_be --network ubuntu_default -d crit_be
