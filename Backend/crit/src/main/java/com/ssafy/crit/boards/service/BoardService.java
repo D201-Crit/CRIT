e935e8b03f10e8a23e596f549d6d1f23d613f212
hyerdd <hyerdd@naver.com>
2023-08-07 14:54:21 +0900

    Debug : 로그 추가

--- Backend/crit/src/main/java/com/ssafy/crit/boards/service/BoardService.java
+++ Backend/crit/src/main/java/com/ssafy/crit/boards/service/BoardService.java
@@ -22,6 +22,7 @@ import com.ssafy.crit.common.s3.S3Uploader;
 
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -34,6 +35,7 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @Service
 @RequiredArgsConstructor
+@Slf4j
 @Transactional
 public class BoardService {
 
@@ -133,6 +135,7 @@ public class BoardService {
 
 
 	public BoardResponseDto update(Long id, BoardResponseDto boardDto, List<MultipartFile> multipartFiles, User user) throws IOException {
+		log.info("Board Id : {}", id);
 		Board board = boardRepository.findById(id).orElseThrow(() -> {
 			return new IllegalArgumentException("Board Id를 찾을 수 없습니다!");
 		});
@@ -140,38 +143,44 @@ public class BoardService {
 		if(!board.getUser().getId().equals(user.getId())){
 			throw new IllegalArgumentException("글 수정권한이 없습니다.");
 		}
+		log.info("2");
 		List<UploadFile> uploadFile = uploadFileRepository.findAllByBoardsId(id);
+		log.info("3");
 
 		uploadFileRepository.deleteAll(uploadFile);
-
+		log.info("4");
 		// Clear and re-add the files to the existing collection.
 		board.getUploadFiles().clear();
-
+		log.info("5");
 		List<String> storeFileResult = new ArrayList<>();
-
+		log.info("6");
 		for (MultipartFile multipartFile : multipartFiles) {
+			log.info("7");
 			if (!multipartFile.isEmpty()) {
-
+				log.info("8");
 				String uploadFiles = s3Uploader.uploadFiles(multipartFile, "Boards");
-
+				log.info("9");
 				UploadFile uploadFileSave = UploadFile.builder()
 					.board(board)
 					.userName(user.getId())
 					.storeFilePath(uploadFiles)
 					.classification(board.getClassification().getCategory())
 					.build();
-
+				log.info("10");
 				uploadFileRepository.save(uploadFileSave);
 				// directly add the new files to the existing collection
+				log.info("11");
 				board.getUploadFiles().add(uploadFileSave);
+				log.info("12");
 				storeFileResult.add(uploadFiles);
+				log.info("13");
 			}
 		}
-
+		log.info("14");
 		board.setUpdate(boardDto.getTitle(),boardDto.getContent());
-
+		log.info("15");
 		boardRepository.save(board);
-
+		log.info("16");
 		return BoardResponseDto.toDto(board);
 	}
 
