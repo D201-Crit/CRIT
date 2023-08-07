97d49c111ad0e9288ce529d5ba9699e8fae760e5
Jinga02 <jjh099@icloud.com>
2023-08-07 11:08:53 +0900

    Feat :  챌린지 시간 범위 변경

--- [FE]프론트/my-app/src/component/challenge/createChallenge/ChallengeTime.js
+++ [FE]프론트/my-app/src/component/challenge/createChallenge/ChallengeTime.js
@@ -15,9 +15,9 @@ const ChallengeTime = ({ onChangeTime }) => {
     const end = new Date(`2000-01-01T${endTime}`);
     const diffInMinutes = Math.abs((end - start) / 1000 / 60);
 
-    // 종료 시간이 시작 시간보다 빠른 경우에만 최대 1시간 조절
-    if (end < start || diffInMinutes > 60) {
-      const adjustedEndTime = new Date(start.getTime() + 60 * 60 * 1000);
+    // 종료 시간이 시작 시간보다 빠른 경우에만 최대 3시간 조절
+    if (end < start || diffInMinutes > 180) {
+      const adjustedEndTime = new Date(start.getTime() + 180 * 60 * 1000);
       setEndTime(adjustedEndTime.toTimeString().slice(0, 5));
     }
     onChangeTime(newStartTime, endTime); // startTime과 endTime을 onChangeTime으로 전달
@@ -32,9 +32,9 @@ const ChallengeTime = ({ onChangeTime }) => {
     const end = new Date(`2000-01-01T${newEndTime}`);
     const diffInMinutes = Math.abs((end - start) / 1000 / 60);
 
-    // 종료 시간이 시작 시간보다 빠른 경우에만 최대 1시간 조절
-    if (end < start || diffInMinutes > 60) {
-      const adjustedStartTime = new Date(end.getTime() - 60 * 60 * 1000);
+    // 종료 시간이 시작 시간보다 빠른 경우에만 최대 3시간 조절
+    if (end < start || diffInMinutes > 180) {
+      const adjustedStartTime = new Date(end.getTime() - 180 * 60 * 1000);
       setStartTime(adjustedStartTime.toTimeString().slice(0, 5));
     }
     onChangeTime(startTime, newEndTime); // startTime과 endTime을 onChangeTime으로 전달
