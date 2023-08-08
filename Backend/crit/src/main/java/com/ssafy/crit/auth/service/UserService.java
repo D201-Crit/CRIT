64bcd4919c7e3b3412ca6e995b7c2eed1602b08a
조경호 <hyerdd@naver.com>
2023-08-08 13:20:46 +0900

    Update Backend/crit/src/main/java/com/ssafy/crit/auth/service/UserService.java

--- Backend/crit/src/main/java/com/ssafy/crit/auth/service/UserService.java
+++ Backend/crit/src/main/java/com/ssafy/crit/auth/service/UserService.java
@@ -72,10 +72,8 @@ public class UserService {
     }
 
     public LogInResponseDto logIn(LogInRequestDto logInRequestDto) throws Exception{
-        log.info("===========================================TEST===========================================");
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         if (!userRepository.existsById(logInRequestDto.getId())) {
-            log.info("Test");
             throw new BadRequestException(ErrorCode.NOT_EXISTS_USER_ID);
         }
         User user = userRepository.findById(logInRequestDto.getId()).get();
