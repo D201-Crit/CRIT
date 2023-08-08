31e58ad1530b67d99eefa6b97a970020bc6878d8
김민석 <koo3435@gmail.com>
2023-08-08 13:48:01 +0900

    Update [FE]프론트/my-app/src/styles/pages/SChallengePage.js

--- [FE]프론트/my-app/src/styles/pages/SChallengePage.js
+++ [FE]프론트/my-app/src/styles/pages/SChallengePage.js
@@ -1,5 +1,10 @@
 import styled from "styled-components";
-import { Swiper, SwiperSlide } from "swiper/react";
+//import { Swiper, SwiperSlide } from "swiper/react";
+import React, { lazy, Suspense } from "react";
+
+// SwiperWrapper와 SwiperSlide를 비동기적으로 불러오기
+const Swiper = lazy(() => import("swiper/react").then(mod => mod.Swiper));
+const SwiperSlide = lazy(() => import("swiper/react").then(mod => mod.SwiperSlide));
 
 // ChallengPage
 export const SCreateChallengeWrapper = styled.div`
