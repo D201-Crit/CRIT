c51a57eac9953fc8f1d095ab92beaefb5c0ba09a
강민승 <k4minseung@gmail.com>
2023-08-10 20:42:00 +0900

    [fix] 금지 목록에서 공백 삭제

--- Backend/crit/src/main/java/com/ssafy/crit/common/global/BannedWords.java
+++ Backend/crit/src/main/java/com/ssafy/crit/common/global/BannedWords.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 @Getter
 @Component
 public class BannedWords {
-	private String[] stt = {" 　", "⠀", "", "⠀", "ㅤ", "---", "▪️", "▪", "◾", "――", "- -", "― ―", "^^ 7", "^ ^ 7",
+	private String[] stt = {"---", "▪️", "▪", "◾", "――", "- -", "― ―", "^^ 7", "^ ^ 7",
 			"^^7", "^7", ";;", "^노^", "￣", "⊥", "─ ─", "━ ━", "──", "━━", "┴", "้", "̆̈", "1빠", "==", "2빠",
 			"D쥐고", "D지고", "jonna", "jot같", "mi쳤", "tlqkf", "&stype=9", "wlfkf", "갈틱폰", "gartic", "같은 새끼",
 			"같은새끼", "개 새끼", "개같아", "개같은", "개같을", "개같게", "개나 소나", "개나대", "개나소나", "개넷", "개년", "개념빠가",
@@ -58,7 +58,7 @@ public class BannedWords {
 			"ㅡㅡ", "ส", "ค็", "้", "็", "zoomin.cur", "eventHostId", "open.", "onchat", "ajax_comment", "stub-extra", "-stub",
 			"line.me", "ask.fm", "adinc.co.kr", "vonvon", "&feature=share", "superfeed", "lumieyes", "theqoo", "dmitory",
 			"_enliple", "apt_review", "showcat", "쇼캣", ".pls", "stype=3", "$('", "bitly", "poomang.com", "comment_memo",
-			"vote.php", "onmouse", "onkeyup", "newsnack", "onkeydown", "", "", "〈"};
+			"vote.php", "onmouse", "onkeyup", "newsnack", "onkeydown", "", "", "〈", "시바라"};
 
 	public boolean isBannedWords(String ext) {
 		return Arrays.asList(stt).contains(ext.toLowerCase());
