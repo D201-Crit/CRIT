1a32bb21e72ed150508f13c3c9d34ab69bf8011e
rlvy98 <rlvy98@naver.com>
2023-08-11 15:52:28 +0900

    fix: Cloud Front에 해당하는 Domain URI 수정

--- Backend/crit/src/main/java/com/ssafy/crit/common/s3/S3Uploader.java
+++ Backend/crit/src/main/java/com/ssafy/crit/common/s3/S3Uploader.java
@@ -64,7 +64,10 @@ public class S3Uploader {
     // S3로 업로드
     private String putS3(File uploadFile, String fileName) {
         amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
-        return amazonS3Client.getUrl(bucket, fileName).toString();
+        return amazonS3Client.getUrl(bucket, fileName).toString(); // S3 업로드된 주소 반환
+        /**
+        * return을 할때 crit service s3 주소기 때문에 fileName만 반환진행 -> 반환한 후에는 CloundFront URI 붙여서 DB에 저장
+         */
     }
 
     // 로컬에 저장된 이미지 지우기
@@ -124,6 +127,9 @@ public class S3Uploader {
             amazonS3Client.putObject(new PutObjectRequest(bucket, thumbnailName, thumbnailFile).withCannedAcl(CannedAccessControlList.PublicRead));
             removeNewFile(thumbnailFile);
             return amazonS3Client.getUrl(bucket, thumbnailName).toString();
+            /**
+             * return을 할때 crit service s3 주소기 때문에 fileName만 반환진행 -> 반환한 후에는 CloundFront URI 붙여서 DB에 저장
+             */
         } catch (JCodecException | IOException e) {
             throw new RuntimeException(e);
         }
