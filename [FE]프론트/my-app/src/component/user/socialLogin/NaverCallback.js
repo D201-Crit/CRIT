fba738d3033a984e621b407f2e805a80943f16e5
rlvy98 <rlvy98@naver.com>
2023-08-14 14:42:51 +0900

    fix: Naver 소셜 로그인 로직 수정

--- [FE]프론트/my-app/src/component/user/socialLogin/NaverCallback.js
+++ [FE]프론트/my-app/src/component/user/socialLogin/NaverCallback.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const NaverCallback = () => {
-  const navigate = useNavigate();
+  const nav = useNavigate();
   const code = new URL(window.location.href).searchParams.get("code");
   const registrationId = "naver";
   const state = "200";
@@ -16,9 +16,32 @@ const NaverCallback = () => {
     })
       .then((res) => {
         console.log(res);
-        localStorage.setItem("token", res.data.accessToken);
-        localStorage.setItem("nickName", res.data.nickname);
-        // navigate("/IntroPage");
+        dispatch(
+          setUser({
+            id: res.data.id,
+            nickname: res.data.nickname,
+            accessToken: res.data.accessToken,
+            refreshToken: res.data.refreshToken,
+          }),
+        );
+        persistor.flush(); // 상태를 영구적으로 저장
+        // Swal.fire({
+        //   position: "center",
+        //   icon: "success",
+        //   title: "로그인 완료",
+        //   text: "CRIT 시작~",
+        //   showConfirmButton: false,
+        //   timer: 1500,
+        //   background: "#272727",
+        //   color: "white",
+        //   // width: "500px",
+        //   // 먼지
+        //   // imageUrl: 'https://unsplash.it/400/200',
+        //   // imageWidth: 400,
+        //   // imageHeight: 200,
+        //   // imageAlt: 'Custom image',
+        // });
+        nav("/MainPage");
       })
       .catch((err) => {
         console.log(err);
