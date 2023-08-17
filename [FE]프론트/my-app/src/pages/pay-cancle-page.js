86619ac8fba23877b03919f83dc8df550540c8a0
Giggle1998 <rlvy98@naver.com>
2023-08-17 23:28:45 +0900

    fix: 디렉토리 수정

--- [FE]프론트/my-app/src/pages/pay-cancle-page.js
+++
@@ -1,9 +0,0 @@
-const PayCanclePage = () => {
-  return (
-    <>
-      <h1>결제 취소</h1>
-    </>
-  );
-};
-
-export default PayCanclePage;
--- [FE]프론트/my-app/src/pages/pay-conflict-page.js
+++
@@ -1,9 +0,0 @@
-const PayConflictPage = () => {
-  return (
-    <>
-      <h1>결제 충돌!!!!</h1>
-    </>
-  );
-};
-
-export default PayConflictPage;
